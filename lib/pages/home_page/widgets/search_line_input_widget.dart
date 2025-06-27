ff40be2a53b2cea2a769fbc69991d1a0d6c56c9e
Emerson <lyncoln_erc@hotmail.com>
2025-06-26 21:52:33 -0300

    [Add] Toggle Search

--- lib/pages/home_page/widgets/search_line_input_widget.dart
+++ lib/pages/home_page/widgets/search_line_input_widget.dart
@@ -1,3 +1,4 @@
+import 'package:animated_toggle/animated_toggle.dart';
 import 'package:flutter/material.dart';
 
 class SearchLineInputWidget extends StatefulWidget {
@@ -11,6 +12,8 @@ class SearchLineInputWidget extends StatefulWidget {
 
 class _SearchLineInputWidgetState extends State<SearchLineInputWidget> {
   final TextEditingController _controller = TextEditingController();
+  int selectedIndex = 0;
+  List<String> options = ['Linha', 'Referência'];
 
   void _onSubmit() {
     final text = _controller.text.trim();
@@ -23,27 +26,74 @@ class _SearchLineInputWidgetState extends State<SearchLineInputWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Expanded(
-          child: TextField(
-            controller: _controller,
-            decoration: InputDecoration(
-              labelText: 'Digite a linha',
-              border: OutlineInputBorder(),
-            ),
-            onSubmitted: (_) => _onSubmit(),
+        Center(
+          child: AnimatedHorizontalToggle(
+            taps: options,
+            width: MediaQuery.of(context).size.width - 100,
+            height: 40,
+            duration: Duration(milliseconds: 200),
+            activeColor: Theme.of(context).colorScheme.secondary,
+            initialIndex: 0,
+            onChange: (currentIndex, targetIndex) {
+              setState(() {
+                selectedIndex = currentIndex;
+              });
+            },
           ),
         ),
-        SizedBox(width: 8),
-        IconButton(
-          icon: Icon(
-            Icons.search,
-            color: Theme.of(context).colorScheme.primary,
-            size: 35,
+        SizedBox(height: 15),
+        if (selectedIndex == 0)
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    labelText: 'Digite a linha',
+                    border: OutlineInputBorder(),
+                  ),
+                  onSubmitted: (_) => _onSubmit(),
+                ),
+              ),
+              SizedBox(width: 8),
+              IconButton(
+                icon: Icon(
+                  Icons.search,
+                  color: Theme.of(context).colorScheme.primary,
+                  size: 35,
+                ),
+                onPressed: _onSubmit,
+              )
+            ],
           ),
-          onPressed: _onSubmit,
-        )
+        if (selectedIndex == 1)
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    labelText: 'Origem',
+                    border: OutlineInputBorder(),
+                  ),
+                  //onSubmitted: (_) => _onSubmit(),
+                ),
+              ),
+              SizedBox(width: 15),
+              Expanded(
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    labelText: 'Destino',
+                    border: OutlineInputBorder(),
+                  ),
+                  //onSubmitted: (_) => _onSubmit(),
+                ),
+              ),
+            ],
+          )
       ],
     );
   }
